e501375728bd0dfc4ea426588e51b2449f955325
Katerina <k_by96@mail.ru>
2023-04-04 22:15:00 +0300

    Task_3 added

---
+++ Task_3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+
+int N = GetNum();
+int GetNum()
+{
+    Console.WriteLine("Input a number: ");
+    while((!int.TryParse(Console.ReadLine(), out N)))
+    {
+        Console.WriteLine("It's not a number");
+    }
+    return N;
+}
+
+string GetCoube()
+{
+    string result = " ";
+    for( int i = 1; i <= N; i++ )
+    {
+        result += Math.Pow(i, 3)+" ";
+    }
+    return result;
+
+}
+Console.Write(GetCoube());
+
+
